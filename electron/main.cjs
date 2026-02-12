2903cfcb0c43af26f5fb891e6c0d1a4e2093a3cd
Rayz <rayz@example.com>
2026-02-12 11:24:28 +0700

    chore: release 1.0.0 with auto-update support

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -24,8 +25,28 @@ function createWindow() {
         // win.webContents.openDevTools();
     } else {
         win.loadFile(path.join(__dirname, '../dist/index.html'));
+        // 僅在生產環境檢查更新
+        autoUpdater.checkForUpdatesAndNotify();
     }
 }
+
+// 自動更新相關事件監聽
+autoUpdater.on('update-available', () => {
+    // 可以在這裡通知渲染進程，或者使用預設的通知
+});
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: '更新已就緒',
+        message: '新版本已下載完成，是否現在重啟並安裝？',
+        buttons: ['現在安裝', '稍後提示']
+    }).then((result) => {
+        if (result.response === 0) {
+            autoUpdater.quitAndInstall();
+        }
+    });
+});
 
 app.whenReady().then(() => {
     createWindow();
